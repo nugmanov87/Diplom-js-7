e195c2b29ba49537e56556218a77c00981a8719d
Ruslan Nugmanov <ruslan-nugmanov87@mail.ru>
2019-10-04 10:51:43 +0500

    версия 2.1 (исправил конструктор объекта)

--- static/main.js
+++ static/main.js
@@ -7,7 +7,7 @@
 Пароль — строка.*/
 
 class Profile {
-    constructor({}) {
+    constructor({username, name: {firstName, lastName}, password}) {
         this.username = username;
         this.name = { firstName, lastName };
         this.password = password;
